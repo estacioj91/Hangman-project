7ca54c28e94dfff14b7b2bc8fbaf286d2183c24d
estacioj91 <estacioj91@yahoo.com>
2018-08-29 14:48:31 -0700

    class syntax
    changed to class syntax

--- js/app.js
+++ js/app.js
@@ -2,12 +2,11 @@ const puzzleElement = document.querySelector("#puzzle");
 const guessesElement = document.querySelector("#guesses");
 const game1 = new Hangman("cat", 2);
 puzzleElement.textContent = game1.getPuzzle();
-guessesElement.textContent = game1.remainingGuesses; 
+guessesElement.textContent = game1.getStatusMessage();
 
 window.addEventListener("keypress", function (event) {
     const guess = String.fromCharCode(event.charCode);
     game1.makeGuess(guess); 
     puzzleElement.textContent = game1.getPuzzle();
-    guessesElement.textContent = game1.remainingGuesses; 
-    console.log(game1.status);
+    guessesElement.textContent = game1.getStatusMessage();
 })
