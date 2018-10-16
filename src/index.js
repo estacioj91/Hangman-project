bc2a7251fd3554489792bceedb338a5e6e3ea48b
estacioj91 <estacioj91@yahoo.com>
2018-10-16 13:53:28 -0700

    Guessed letters
    guessed letters are now shown

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import Hangman from "./hangman"
 import getPuzzle from "./request"
 const puzzleElement = document.querySelector(".puzzle");
 const guessesElement = document.querySelector("#guesses");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 let game1;
 
 window.addEventListener("keypress", (event) => {
-    const guess = String.fromCharCode(event.charCode);
-    game1.makeGuess(guess);
-    render();
+    if (event.charCode != 32) {
+        const guess = String.fromCharCode(event.charCode);
+        game1.makeGuess(guess);
+        render();
+    }
 })
 
 const startGame = async () => {
@@ -24,6 +27,16 @@ const render = () => {
         letterElement.textContent = letter;
         puzzleElement.appendChild(letterElement);
     });
+    guessedLettersElement.innerHTML = "";
+    game1.guessedLetters.forEach(letter => {
+        console.log(letter);
+        if (letter != "") {
+            const guessedletter = document.createElement("span");
+            guessedletter.textContent = letter;
+            guessedLettersElement.appendChild(guessedletter);
+        }
+    });
+
 }
 startGame();
 document.querySelector("#reset").addEventListener("click", startGame);
